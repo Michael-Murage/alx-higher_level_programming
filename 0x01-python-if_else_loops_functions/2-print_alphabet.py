cba44d87b9bc57e3cdffcf73ec8b6474373040cd
Michael <mickeymurage@gmail.com>
2022-11-30 10:06:03 +0300

    fixed file naming errors

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 122):
+for letter in range(97, 123):
     if chr(letter):
         print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-print('00', end='')
-for i in range(1, 100):
-    print(", {:0>2}".format(i), end='')
-print("\n")
+# print('00', end='')
+for i in range(0, 100):
+    if (i == 99):
+        print("99")
+    else:
+        print("{:0>2}".format(i), end=', ')
