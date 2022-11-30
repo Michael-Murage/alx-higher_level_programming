b30a49172a46d46423afb019ecf75fe0972029c1
Michael <mickeymurage@gmail.com>
2022-11-30 10:09:33 +0300

    deleted extra new line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,11 @@ y = int(str(number)[-1])
 def compare(num):
     x = int(str(num)[-1])
     if (x > 5):
-        return "and is greater than 5\n"
+        return "and is greater than 5"
     elif (x == 0):
-        return "and is 0\n"
+        return "and is 0"
     elif (x > 0 & x <= 5):
-        return "and is less than 6 and not 0\n"
+        return "and is less than 6 and not 0"
 
 
 print(f"Last digit of {number} is {y} {compare(number)}")
