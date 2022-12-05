bb2cf2c24bb34c11d77447085ade523842366329
Michael <mickeymurage@gmail.com>
2022-12-05 23:25:21 +0300

    fixed for loop in number 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     bool_result = []
-    for i in my_list:
+    for i in range(len(my_list)):
         if (my_list[i] % 2 == 0):
             bool_result.append(True)
         else:
