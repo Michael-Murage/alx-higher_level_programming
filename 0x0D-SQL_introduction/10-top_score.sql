199eef4e645c57f368dfc5214e2a0adcbec6657c
Michael <mickeymurage@gmail.com>
2023-02-15 04:33:27 +0300

    average, groups and no link

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- lists all records of the table
 --  second_table of the database hbtn_0c_0
-SELECT second_table.score, second_table.name FROM second_table ORDER BY score;
+SELECT score, name FROM second_table ORDER BY score;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table second_table in the database 
 --  in your MySQL server and add multiples rows
 CREATE TABLE IF NOT EXISTS second_table (id INT AUTOINCREMENT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id=?, name=?, score=?) VALUES (1, "John", 10);
-INSERT INTO second_table (id=?, name=?, score=?) VALUES (2, "Alex", 3);
-INSERT INTO second_table (id=?, name=?, score=?) VALUES (3, "Bob", 14);
-INSERT INTO second_table (id=?, name=?, score=?) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
