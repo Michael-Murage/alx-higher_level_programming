112b6cae81a73b90bc2312a1f29207968630e220
Michael <mickeymurage@gmail.com>
2023-02-16 23:30:55 +0300

    genre id all shows, no genre and count shows by genre

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,6 +4,5 @@
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states 
-ON states.id = cities.state_id ORDER BY cities.id;
+ON states.id = cities.state_id ORDER BY cities.id ASC;
