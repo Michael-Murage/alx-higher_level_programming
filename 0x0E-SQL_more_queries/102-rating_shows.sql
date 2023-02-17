234663489274f126df6739fd287dd35e80ad6127
Michael <mickeymurage@gmail.com>
2023-02-17 05:49:20 +0300

    not my genres, not a comedy, rating shows and rating genres plus the database dumps being used

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT title, SUM(tv_show_ratings.rate) 'rating' FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title ORDER BY rating DESC;
