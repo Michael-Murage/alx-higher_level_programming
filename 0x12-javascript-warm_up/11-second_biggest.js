eea7b94e253b8a35e2cd7eb39683fa09b8f92042
Michael <mickeymurage@gmail.com>
2023-03-25 14:56:05 +0300

    add,  factorial and second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log(a)
+const args = process.argv;
+
+if (args.length <= 3) {
+  console.log(0);
+} else {
+  const arr = args.slice(2).map((a) => parseInt(a));
+  const secondLargest = arr.sort((a, b) => b - a)[1];
+  console.log(secondLargest);
+}
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv;
+
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+console.log(add(args[2], args[3]));
