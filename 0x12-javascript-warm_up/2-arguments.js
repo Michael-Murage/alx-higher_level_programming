8ce6c4f5ec51bfee4897d9af8c3772bc42d25cbb
Michael <mickeymurage@gmail.com>
2023-03-25 11:09:59 +0300

    fixed some semistandard bugs

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args.length <= 2) {
+  console.log("No argument");
+} else if (args.length === 3) {
+  console.log("Argument found");
+} else if (args.length > 3) {
+  console.log("Arguments found");
+}
