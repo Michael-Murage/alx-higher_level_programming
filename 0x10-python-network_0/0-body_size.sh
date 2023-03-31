2c53a40b47a6123c9fdc72e890c61317dc3a0e06
Michael <mickeymurage@gmail.com>
2023-03-31 19:33:38 +0300

    body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
 # The size must be displayed in bytes
 # You have to use curl
-curl -sI "$1" | grep "Content-Length" | awk "{print $2}"
+curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
