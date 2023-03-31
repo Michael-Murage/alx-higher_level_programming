d02b7122d7eb129f3f2a95a5e9dc4681343fedf6
Michael <mickeymurage@gmail.com>
2023-04-01 01:24:35 +0300

    delete, methods and header

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# takes in a URL as an argument, sends a GET
+#  request to the URL, and displays the body of the response
+# A header variable X-School-User-Id must be sent with the value 98
+# You have to use curl
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
