3853a27060e8364c4c0e068cc8521ad4061bf458
Michael <mickeymurage@gmail.com>
2023-04-02 22:11:47 +0300

    status code, post json and catch me

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,7 +15,7 @@ def find_peak(list_of_integers):
     if high == 2:
         return max(list_of_integers)
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-        list_of_integers[mid] >= list_of_integers[mid + 1]:
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:])
