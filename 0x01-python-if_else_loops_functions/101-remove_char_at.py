6a9440bda76e74480338218d58d6a172bb436d54
Michael <mickeymurage@gmail.com>
2023-04-23 06:42:31 +0300

    remove char at correction 1

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 creates a copy of the string, removing the character
  at the position n (not the Python way, the “C array index”).
 """
+
 
 def remove_char_at(str, n):
     if n >= len(str) or n < 0:
